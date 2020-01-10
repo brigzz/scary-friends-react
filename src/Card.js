078cccea1f2d4d83d03e3cb4d94e6062210a1513
Eugho <eugholassie@gmail.com>
2020-01-10 20:06:06 +0800

    1st Robot Commit

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='robots' src=''/>
+            <div>
+                <h2>Eugene Ho</h2>
+                <p>briglaz@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
