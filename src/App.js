1afa5b444f89fad551c6b4972aafd4323919177d
Eugho <eugholassie@gmail.com>
2020-01-10 19:33:58 +0800

    clean up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Hello from './Hello';
-import logo from './logo.svg';
+import './App.css';
 
 
 class App extends Component {
--- src/Hello.js
+++ src/Hello.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import './Hello.css';
 
 class Hello extends Component {
     render() {
         return (
-            <div>
+            <div className='f1 tc shadow-3 bg-gold'>
                 <h1>Hello World!</h1>
             </div>
         )
