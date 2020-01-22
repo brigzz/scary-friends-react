decb06cada7903795c7ef36c1e6dce5b250da6bc
Eugho <eugholassie@gmail.com>
2020-01-23 02:21:13 +0800

    Updates

---
+++ precache-manifest.d4f21fa75c447d71d82fb91584c916c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a10d1277d4e3082bd2677f6c6a64b19b",
+    "url": "/scary-friends-react/index.html"
+  },
+  {
+    "revision": "62a4431077fb3dd4ca33",
+    "url": "/scary-friends-react/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "278a5624b8475681cbda",
+    "url": "/scary-friends-react/static/css/main.cb3aac60.chunk.css"
+  },
+  {
+    "revision": "62a4431077fb3dd4ca33",
+    "url": "/scary-friends-react/static/js/2.25cf1031.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/scary-friends-react/static/js/2.25cf1031.chunk.js.LICENSE"
+  },
+  {
+    "revision": "278a5624b8475681cbda",
+    "url": "/scary-friends-react/static/js/main.2237b104.chunk.js"
+  },
+  {
+    "revision": "44ecf4ff3f4d70a71594",
+    "url": "/scary-friends-react/static/js/runtime-main.5e35ac1a.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/scary-friends-react/static/media/SEGA.82e09f78.TTF"
+  }
+]);
